d1be9febd292a661b7759761e3d9682f38d6e24c
johanna.zambrano <johanna.zambrano@eng.it>
2025-08-30 16:00:45 +0200

    Correzione fetch e gestione stato in ListaSpese

--- src/components/ListaSpese.js
+++ src/components/ListaSpese.js
@@ -8,9 +8,6 @@ function ListaSpese() {
         // Chiamata all'API del backend
         fetch('http://localhost:8080/api/spese')
             .then(response => response.json()) //converte la response in un json
-            .then(data => {
-                console.log("Risultato della fetch:", data);
-            })
             .then(data => setSpese(data)) //con i dati convertiti in json questa parte li prende data e usa la funzione setSpese per aggiornare lo stato spese
             .catch(error => console.error('Errore nel recupero delle spese:', error));
     }, []); // L'array vuoto alla fine è cruciale, assicura che il codice venga eseguito solo una volta quando il componente appare per la prima volta
